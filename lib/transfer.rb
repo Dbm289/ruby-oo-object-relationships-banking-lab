321e8ef006b0da175dc397079b5a68eafcbb99c3
Dan <DanielMooreRealPerson@gmail.com>
2020-12-16 17:25:00 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :amount, :sender, :receiver, :status
+  
+  def initialize( sender, receiver, amount)
+    
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+    
+
+  end
+
+  def valid?
+    @sender = "open" && @receiver == "open"
+
+  end
+
 end
